61d3c1ac20603aefde335ac02b4ccb82bc54409e
David71-star <davidcortes024@gmail.com>
2023-10-10 15:28:46 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// Cosa è un algoritmo ?
+// Un algoritmo si può definire come una ricetta, è una sequenza di istruzioni (step) "finita" che, seguite nel giusto
+// ordine, partendo da specifci input (gli ingredienti), consente di ottenere un determinato risultato, ovvero,
+// trasformare l'input nell'output desiderato (piatto desiderato).
+
+// Cosa è una variabile ?
+// una variabile è un (contenitore) alla quale diamo un nome e associamo un valore,
+// come un testo o un numero, per utilizzarlo e memorizzarlo durante l'esecuzione del codice.
+// Rimanendo nel tema cucina le variabili sono proprio i contenitori (ciotole, vassoi etc..)che servono per conservare
+// temporaneamente risultati parziali ( impasto, salse etc..).
+
+// Undefined e null sono la stessa cosa ?
+// Risposta: Falso.
+// Undefined indica una mancanza totale dell'informazione, la variabile stessa non esiste.
+// Null è un'iformazione più specifica, significa che il valore di questa varialibe non è assegnato.
+
